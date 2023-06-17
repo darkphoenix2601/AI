298a45f17918da42b1aa7b4348cd339c6015c289
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2023-06-17 14:15:27 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,37 +2,37 @@ import numpy as np
 import json
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset,DataLoader
-from Nueralnetwork import bag_of_words,tokenize,stem
-from brain import NueralNet
-        
-with open('intents.json','r')as f:
-    intents= json.load(f)
-all_words=[]
-tags=[]
-xy=[]
+from torch.utils.data import Dataset, DataLoader
+from Neuralnetwork import bag_of_words, tokenize, stem
+from brain import NeuralNet
+
+# Load intents from JSON file
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
 
 for intent in intents['intents']:
-    tag= intent['tag']
+    tag = intent['tag']
     tags.append(tag)
     
     for pattern in intent['patterns']:
         print(pattern)
-        w=tokenize(pattern)
+        w = tokenize(pattern)
         all_words.extend(w)
-        xy.append((w,tag))
+        xy.append((w, tag))
 
-ignore_words=[',','?','/','.','!']
-all_words=[stem(w)for w in all_words if w not in ignore_words]
-all_words=sorted(set(all_words))
-tags=sorted(set(tags))
-x_train=[]
+ignore_words = [',', '?', '/', '.', '!']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+x_train = []
+y_train = []
 
-y_train=[]
-
-
-for (pattern_sentence,tag)in xy:
-    bag = bag_of_words(pattern_sentence,all_words)
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
     x_train.append(bag)
 
     label = tags.index(tag)
@@ -40,57 +40,52 @@ for (pattern_sentence,tag)in xy:
 
 x_train = np.array(x_train)
 y_train = np.array(y_train)
-    
+
 num_epochs = 1000
 batch_size = 8
-learning_rate=0.001
-input_size=len(x_train[0])
-hidden_size=8
-output_size=len(tags)
+learning_rate = 0.001
+input_size = len(x_train[0])
+hidden_size = 8
+output_size = len(tags)
 
-print("training")
+print("Training")
 
 class ChatDataset(Dataset):
     def __init__(self):
-        
-        self.n_samples= len(x_train)
+        self.n_samples = len(x_train)
         self.x_data = x_train
         self.y_data = y_train
 
-    def __getiem__(self,index):
-        return self.x_data[index],self.y_data[index]
-    
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
     
     def __len__(self):
         return self.n_samples
-    
 
-    
 dataset = ChatDataset()
 
-train_loader= DataLoader(dataset=dataset,
-                           batch_size=batch_size,
-                           shuffle =True,
-                           num_workers=0)
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
 
-
-device = torch.device('cuda' if torch.cuda.is_available()else 'cpu')
-model = NueralNet(input_size,hidden_size,output_size).to(device=device)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size, hidden_size, output_size).to(device=device)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 for epoch in range(num_epochs):
-    for(words,labels)in train_loader:
-        words= words.to(device)
-        labels=labels.to(dtype=torch.long).to(device)
+    for words, labels in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
         outputs = model(words)
-        loss=criterion(outputs,labels)
+        loss = criterion(outputs, labels)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    if (epoch+1) %100==0:
-        print(f'Epoch[{epoch+1}/{num_epochs}],Loss:{loss.item():.4f}')
+        
+    if (epoch + 1) % 100 == 0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
-    print(f'Final loss:{loss.item():.3f}')
-
+print(f'Final loss: {loss.item():.3f}')
