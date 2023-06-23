f9132ea1aae1d22598f42de84e2c24afde74c662
dark phoenix <akshit2022@outlook.com>
2023-06-23 14:27:28 +0530

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,40 @@
+import openai
+
+# Set up your OpenAI API key
+openai.api_key = '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'
+
+# Function to ask the chatbot a question and get a response
+def ask_chatbot(question, chat_log=[]):
+    # Append user's question to the chat log
+    chat_log.append(question)
+
+    # Generate a response from OpenAI API
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt='\n'.join(chat_log),
+        temperature=0.7,
+        max_tokens=100,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+
+    # Retrieve and return the chatbot's reply
+    reply = response.choices[0].text.strip()
+    chat_log.append(reply)
+    return reply
+
+# Main loop
+chat_log = []
+
+while True:
+    user_input = input("User: ")
+
+    # Check if the user wants to exit
+    if user_input.lower() == "exit":
+        break
+
+    # Get a response from the chatbot
+    bot_response = ask_chatbot(user_input, chat_log)
+    print("Chatbot:", bot_response)
+
